d74ee5a9f6e8b7ef34798d8fa840562b3804dd07
Felix <jonfelixrico@gmail.com>
2022-09-10 19:05:22 +0800

    Add revision

--- src/main/java/com/wisdom/writemodel/common/EventsBuffer.java
+++ src/main/java/com/wisdom/writemodel/common/EventsBuffer.java
@@ -1,5 +1,6 @@
 package com.wisdom.writemodel.common;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,12 +9,18 @@ import com.wisdom.eventsourcing.Event;
 public class EventsBuffer {
 	private String streamId;
 	private List<Event> events;
+	private BigInteger revision;
 
 	public EventsBuffer(String streamId) {
 		this.streamId = streamId;
 		this.events = new ArrayList<>();
 	}
-	
+
+	public EventsBuffer(String streamId, BigInteger revision) {
+		this(streamId);
+		this.revision = revision;
+	}
+
 	public String getStreamId() {
 		return streamId;
 	}
@@ -21,7 +28,11 @@ public class EventsBuffer {
 	public List<Event> getEvents() {
 		return List.copyOf(events);
 	}
-	
+
+	public BigInteger getRevision() {
+		return revision;
+	}
+
 	public void pushEvent(Event event) {
 		events.add(event);
 	}
