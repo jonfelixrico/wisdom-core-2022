e65c5b8af23f4fb0aeefd98aae3e01210462a388
Felix <jonfelixrico@gmail.com>
2022-09-10 19:11:12 +0800

    Add constructor to QuoteReceivedEvent

--- src/main/java/com/wisdom/quote/aggregate/events/QuoteReceivedEvent.java
+++ src/main/java/com/wisdom/quote/aggregate/events/QuoteReceivedEvent.java
@@ -16,6 +16,17 @@ public class QuoteReceivedEvent implements Event {
 	private String channelId;
 	private String messageId;
 
+	public QuoteReceivedEvent(String quoteId, String receiveId, String userId, Instant createDt, String serverId,
+			String channelId, String messageId) {
+		this.quoteId = quoteId;
+		this.receiveId = receiveId;
+		this.userId = userId;
+		this.createDt = createDt;
+		this.serverId = serverId;
+		this.channelId = channelId;
+		this.messageId = messageId;
+	}
+
 	public String getQuoteId() {
 		return quoteId;
 	}
