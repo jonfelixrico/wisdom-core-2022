3d45770324445c0966d9141ba245becb4f69c385
Felix <jonfelixrico@gmail.com>
2022-09-10 19:10:02 +0800

    Add quoteId to approved events

--- src/main/java/com/wisdom/quote/aggregate/events/QuoteApprovedBySystemEvent.java
+++ src/main/java/com/wisdom/quote/aggregate/events/QuoteApprovedBySystemEvent.java
@@ -5,10 +5,16 @@ import java.time.Instant;
 import com.wisdom.eventsourcing.Event;
 
 public class QuoteApprovedBySystemEvent implements Event {
+	private String quoteId;
 	private Instant timestamp;
 
-	public QuoteApprovedBySystemEvent(Instant timestamp) {
+	public QuoteApprovedBySystemEvent(String quoteId, Instant timestamp) {
+		this.quoteId = quoteId;
 		this.timestamp = timestamp;
+	}
+
+	public String getQuoteId() {
+		return quoteId;
 	}
 
 	public Instant getTimestamp() {
--- src/main/java/com/wisdom/quote/aggregate/events/QuoteFlaggedAsExpiredBySystemEvent.java
+++ src/main/java/com/wisdom/quote/aggregate/events/QuoteFlaggedAsExpiredBySystemEvent.java
@@ -5,10 +5,16 @@ import java.time.Instant;
 import com.wisdom.eventsourcing.Event;
 
 public class QuoteFlaggedAsExpiredBySystemEvent implements Event {
+	private String quoteId;
 	private Instant timestamp;
 
-	public QuoteFlaggedAsExpiredBySystemEvent(Instant timestamp) {
+	public QuoteFlaggedAsExpiredBySystemEvent(String quoteId, Instant timestamp) {
+		this.quoteId = quoteId;
 		this.timestamp = timestamp;
+	}
+
+	public String getQuoteId() {
+		return quoteId;
 	}
 
 	public Instant getTimestamp() {
