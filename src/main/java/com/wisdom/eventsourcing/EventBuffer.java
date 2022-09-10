e7ccb72c831ff0947c0bc9a107e5cded9a1c4de8
Felix <jonfelixrico@gmail.com>
2022-09-10 20:03:00 +0800

    Add comments

--- src/main/java/com/wisdom/eventsourcing/EventBuffer.java
+++ src/main/java/com/wisdom/eventsourcing/EventBuffer.java
@@ -4,16 +4,34 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This is a class that helps with pushing several events to the same stream.
+ * This is very specific to EventSourceDB.
+ * 
+ * @author Felix
+ *
+ */
 public class EventBuffer {
 	private String streamId;
 	private List<Event> events;
+	
 	private BigInteger revision;
 
+	/**
+	 * This will create an EventBuffer without an expected revision.
+	 * @param streamId
+	 */
 	public EventBuffer(String streamId) {
 		this.streamId = streamId;
 		this.events = new ArrayList<>();
 	}
 
+	/**
+	 * This will create an EventBuffer that expects the last stream revision
+	 * id to be `revision`.
+	 * @param streamId
+	 * @param revision
+	 */
 	public EventBuffer(String streamId, BigInteger revision) {
 		this(streamId);
 		this.revision = revision;
@@ -23,6 +41,10 @@ public class EventBuffer {
 		return streamId;
 	}
 
+	/**
+	 * Returns an immutable shallow copy of the event sequence built.
+	 * @return
+	 */
 	public List<Event> getEvents() {
 		return List.copyOf(events);
 	}
