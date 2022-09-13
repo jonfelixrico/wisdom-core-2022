761ff3688eff176922a17890f3319843cee762e6
Felix <jonfelixrico@gmail.com>
2022-09-14 02:04:59 +0800

    Auto-obtain the client on startup

--- src/main/java/com/wisdom/eventstoredb/EventStoreDBProvider.java
+++ src/main/java/com/wisdom/eventstoredb/EventStoreDBProvider.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import com.eventstore.dbclient.EventStoreDBClient;
@@ -25,6 +27,10 @@ public class EventStoreDBProvider {
 
 	private CompletableFuture<EventStoreDBClient> clientCf;
 
+	/**
+	 * This tries to obtain an ESDB client instance asynchronously.
+	 * @return
+	 */
 	private CompletableFuture<EventStoreDBClient> createClientCf() {
 		return CompletableFuture.supplyAsync(() -> {
 			LOGGER.info("Asynchronously trying to connect to ESDB");
@@ -42,5 +48,21 @@ public class EventStoreDBProvider {
 		}
 		
 		return clientCf.get();
+	}
+	
+	/**
+	 * This tries to retrieve the ESDB client instance once the application has started.
+	 */
+	@EventListener(ContextStartedEvent.class)
+	private void attemptToRetrieveClient () {
+		LOGGER.debug("Startup detected, attempting to get ESDB client instance.");
+		
+		if (clientCf != null) {
+			LOGGER.warn("CompletableFuture for the client has already been created shortly after startup. This shouldn't be happening...");
+			return;
+		}
+		
+		clientCf = createClientCf();
+		LOGGER.debug("CompletableFuture for the client has been created.");
 	}
 }
