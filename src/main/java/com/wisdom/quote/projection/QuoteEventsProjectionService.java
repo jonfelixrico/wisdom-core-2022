c4665a23fd84d0fb5607d63ca702101f0a7dd62d
Felix <jonfelixrico@gmail.com>
2022-09-15 21:53:15 +0800

    Remove unused imports

--- src/main/java/com/wisdom/quote/projection/QuoteEventsProjectionService.java
+++ src/main/java/com/wisdom/quote/projection/QuoteEventsProjectionService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
