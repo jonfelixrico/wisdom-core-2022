e5b549c0f5eafa9e31cce378e9faa0c9bc3dd41c
Felix <jonfelixrico@gmail.com>
2022-09-17 00:39:36 +0800

    Hard-code vote requirement of 3

--- src/main/java/com/wisdom/quote/writemodel/QuoteWriteModelRepository.java
+++ src/main/java/com/wisdom/quote/writemodel/QuoteWriteModelRepository.java
@@ -23,9 +23,10 @@ public class QuoteWriteModelRepository {
 	EventAppendService eventAppendService;
 
 	public QuoteWriteModel create(String quoteId, String content, String authorId, String submitterId, Instant createDt,
-			Instant expirationDt, String serverId, String channelId, String messageId, int requiredVoteCount) {
+			Instant expirationDt, String serverId, String channelId, String messageId) {
 		var model = new QuoteWriteModel(expirationDt, new ArrayList<>(), new ArrayList<>(), null, quoteId,
-				requiredVoteCount, ExpectedRevision.NO_STREAM);
+				// TODO change the value to something from the DB
+				3, ExpectedRevision.NO_STREAM);
 
 		model.getEventBuffer().pushEvent(new QuoteSubmittedEvent(quoteId, content, authorId, submitterId, createDt,
 				expirationDt, serverId, channelId, messageId, 3)); // TODO adjust the required vote count
@@ -37,7 +38,7 @@ public class QuoteWriteModelRepository {
 	public QuoteWriteModel getWriteModel(String quoteId) throws InterruptedException, ExecutionException, IOException {
 		var result = projectionService.getProjection(quoteId);
 		var data = result.getFirst();
-		
+
 		var receives = data.getReceives().stream().map(r -> r.getId()).collect(Collectors.toList());
 		return new QuoteWriteModel(data.getExpirationDt(), data.getVoterIds(), receives, data.getVerdict(),
 				data.getId(), data.getRequiredVoteCount(), ExpectedRevision.expectedRevision(result.getSecond()));
