41d30d136c2559bd504f71026c29cad37ef068ec
Felix <jonfelixrico@gmail.com>
2022-09-16 23:33:52 +0800

    Remove unused imports

--- src/main/java/com/wisdom/quote/projection/QuoteProjectionService.java
+++ src/main/java/com/wisdom/quote/projection/QuoteProjectionService.java
@@ -1,10 +1,8 @@
 package com.wisdom.quote.projection;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
