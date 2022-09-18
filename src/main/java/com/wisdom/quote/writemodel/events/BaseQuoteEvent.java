30b18e11d6f61e194203f1c7aa10ffb66e5686b7
Felix <jonfelixrico@gmail.com>
2022-09-18 21:07:52 +0800

    Add getQuoteId to the BaseQuoteEvent

--- src/main/java/com/wisdom/quote/writemodel/events/BaseQuoteEvent.java
+++ src/main/java/com/wisdom/quote/writemodel/events/BaseQuoteEvent.java
@@ -3,4 +3,5 @@ package com.wisdom.quote.writemodel.events;
 import com.wisdom.eventstoredb.utils.Event;
 
 public abstract class BaseQuoteEvent implements Event {
+	public abstract String getQuoteId();
 }
