3f7c5a725e87438f8b25c6a33eb7a48bbecab85c
Felix <jonfelixrico@gmail.com>
2022-09-18 19:34:46 +0800

    Delete the common snapshot package

--- src/main/java/com/wisdom/common/projection/snapshot/ProjectionMetaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.wisdom.common.projection.snapshot;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-interface ProjectionMetaRepository extends MongoRepository<ProjectionMetaMongoModel, String> {
-}
