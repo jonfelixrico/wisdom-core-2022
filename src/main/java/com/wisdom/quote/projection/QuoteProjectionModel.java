22d218784b72d54f191fb5e63ff5c9d7dc242ff5
Felix <jonfelixrico@gmail.com>
2022-09-18 17:10:29 +0800

    Make abstract version of QuoteProjectionModel

--- src/main/java/com/wisdom/quote/projection/QuoteProjectionModel.java
+++ src/main/java/com/wisdom/quote/projection/QuoteProjectionModel.java
@@ -5,94 +5,12 @@ import java.util.List;
 
 import com.wisdom.quote.aggregate.Verdict;
 
-public class QuoteProjectionModel {
-	private String id;
-
-	private String content;
-	private String authorId;
-	private String submitterId;
-
-	private Instant submitDt;
-	private Instant expirationDt;
-
-	private String serverId;
-	private String channelId;
-	private String messageId;
-
-	private List<String> voterIds;
-	private List<Receive> receives;
-	private Verdict verdict;
-
-	private Integer requiredVoteCount;
+public class QuoteProjectionModel extends BaseQuoteProjectionModel {
 
 	public QuoteProjectionModel(String id, String content, String authorId, String submitterId, Instant submitDt,
 			Instant expirationDt, String serverId, String channelId, String messageId, List<String> voterIds,
 			List<Receive> receives, Verdict verdict, Integer requiredVoteCount) {
-		this.id = id;
-		this.content = content;
-		this.authorId = authorId;
-		this.submitterId = submitterId;
-		this.submitDt = submitDt;
-		this.expirationDt = expirationDt;
-		this.serverId = serverId;
-		this.channelId = channelId;
-		this.messageId = messageId;
-		this.voterIds = voterIds;
-		this.receives = receives;
-		this.verdict = verdict;
-		this.requiredVoteCount = requiredVoteCount;
+		super(id, content, authorId, submitterId, submitDt, expirationDt, serverId, channelId, messageId, voterIds,
+				receives, verdict, requiredVoteCount);
 	}
-
-	public Integer getRequiredVoteCount() {
-		return requiredVoteCount;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public String getAuthorId() {
-		return authorId;
-	}
-
-	public String getSubmitterId() {
-		return submitterId;
-	}
-
-	public Instant getSubmitDt() {
-		return submitDt;
-	}
-
-	public Instant getExpirationDt() {
-		return expirationDt;
-	}
-
-	public String getServerId() {
-		return serverId;
-	}
-
-	public String getChannelId() {
-		return channelId;
-	}
-
-	public String getMessageId() {
-		return messageId;
-	}
-
-	public List<String> getVoterIds() {
-		return voterIds;
-	}
-
-	public List<Receive> getReceives() {
-		return receives;
-	}
-
-	public Verdict getVerdict() {
-		return verdict;
-	}
-
 }
