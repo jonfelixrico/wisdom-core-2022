4830b54c88b59edbc03c16e57377d14e6e6a16cb
Felix <jonfelixrico@gmail.com>
2022-09-22 01:00:37 +0800

    Rename variables

--- src/main/java/com/wisdom/quote/controller/ServerPendingQuotesController.java
+++ src/main/java/com/wisdom/quote/controller/ServerPendingQuotesController.java
@@ -35,10 +35,10 @@ import com.wisdom.quote.writemodel.QuoteWriteModelRepository;
 @RequestMapping("/server/{serverId}/quote/pending")
 public class ServerPendingQuotesController {
 	@Autowired
-	QuoteWriteModelRepository writeRepository;
+	QuoteWriteModelRepository writeRepo;
 
 	@Autowired
-	PendingQuoteService readRepo;
+	PendingQuoteService pendingQuoteSvc;
 
 	@PostMapping
 	private Map<String, String> submitQuote(@Valid @RequestBody SubmitQuoteReqDto body) throws Exception {
@@ -46,10 +46,10 @@ public class ServerPendingQuotesController {
 		var createDt = Instant.now();
 		var expireDt = createDt.plus(3, ChronoUnit.DAYS);
 
-		var writeModel = writeRepository.create(quoteId, body.getContent(), body.getAuthorId(), body.getSubmitterId(),
+		var writeModel = writeRepo.create(quoteId, body.getContent(), body.getAuthorId(), body.getSubmitterId(),
 				createDt, expireDt, body.getServerId(), body.getChannelId(), body.getMessageId());
 
-		writeRepository.saveWriteModel(writeModel);
+		writeRepo.saveWriteModel(writeModel);
 
 		return Map.of("quoteId", quoteId);
 	}
@@ -57,19 +57,19 @@ public class ServerPendingQuotesController {
 	@PutMapping("/{id}/vote")
 	private void setVotes(@PathVariable String id, @RequestBody List<String> voterIds)
 			throws InterruptedException, ExecutionException, IOException {
-		var model = writeRepository.getWriteModel(id);
+		var model = writeRepo.getWriteModel(id);
 		model.setVoters(voterIds, Instant.now());
-		writeRepository.saveWriteModel(model);
+		writeRepo.saveWriteModel(model);
 	}
 
 	@GetMapping("/{id}")
 	private PendingQuoteReadModel getPendingQuote(@PathVariable String serverId, @PathVariable String id) {
-		return this.readRepo.getPendingQuote(id, serverId);
+		return this.pendingQuoteSvc.getPendingQuote(id, serverId);
 	}
 
 	@GetMapping
 	private List<PendingQuoteReadModel> getPendingQuotes(@PathVariable String serverId) {
-		return this.readRepo.getPendingQuotes(serverId);
+		return this.pendingQuoteSvc.getPendingQuotes(serverId);
 	}
 
 }
