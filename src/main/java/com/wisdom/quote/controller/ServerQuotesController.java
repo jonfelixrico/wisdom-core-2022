ad5efe290940deb03d9a60494fd3815dda19b4a8
Felix <jonfelixrico@gmail.com>
2022-09-22 20:38:25 +0800

    Create controller for approved quotes

---
+++ src/main/java/com/wisdom/quote/controller/ServerQuotesController.java
@@ -0,0 +1,22 @@
+package com.wisdom.quote.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wisdom.quote.service.QuoteService;
+import com.wisdom.quote.service.QuoteServiceModel;
+
+@RestController
+@RequestMapping("/server/{serverId}/quote")
+public class ServerQuotesController {
+	@Autowired
+	private QuoteService svc;
+
+	@GetMapping
+	private QuoteServiceModel getRandomQuote(@PathVariable String serverId) {
+		return svc.getRandomQuote(serverId);
+	}
+}
