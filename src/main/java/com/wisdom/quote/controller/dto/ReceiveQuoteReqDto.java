a98b2663bf1db14beaed88acc3e3b632b616cc9e
Felix <jonfelixrico@gmail.com>
2022-09-29 00:03:28 +0800

    Add data validation

--- src/main/java/com/wisdom/quote/controller/dto/ReceiveQuoteReqDto.java
+++ src/main/java/com/wisdom/quote/controller/dto/ReceiveQuoteReqDto.java
@@ -1,10 +1,23 @@
 package com.wisdom.quote.controller.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class ReceiveQuoteReqDto {
+	@NotNull
+	@NotBlank
 	private String channelId;
+	
+	@NotNull
+	@NotBlank
 	private String messageId;
 
+	@NotNull
+	@NotBlank
 	private String quoteId;
+	
+	@NotNull
+	@NotBlank
 	private String receivedBy;
 
 	public String getChannelId() {
