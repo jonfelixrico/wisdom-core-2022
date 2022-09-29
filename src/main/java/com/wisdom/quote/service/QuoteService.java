cd2ba3db18a46c4f917048d6a4c257e270ec4013
Felix <jonfelixrico@gmail.com>
2022-09-29 22:57:09 +0800

    Push implementation for QuoteService#receiveQuote

--- src/main/java/com/wisdom/quote/service/QuoteService.java
+++ src/main/java/com/wisdom/quote/service/QuoteService.java
@@ -1,31 +1,49 @@
 package com.wisdom.quote.service;
 
+import java.io.IOException;
+import java.time.Instant;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wisdom.quote.projection.QuoteProjectionService;
 import com.wisdom.quote.projection.snapshot.QuoteMongoRepository;
+import com.wisdom.quote.writemodel.QuoteWriteModelRepository;
 
 @Service
 public class QuoteService {
 	@Autowired
 	private QuoteMongoRepository repo;
+	private QuoteWriteModelRepository writeRepo;
+	private QuoteProjectionService projSvc;
 
 	public QuoteServiceModel getRandomQuote(String serverId) {
 		var results = repo.getRandomQuote(serverId);
 		if (results.size() == 0) {
 			return null;
 		}
-		
+
 		return new QuoteServiceModel(results.get(0));
 	}
-	
-	public void receiveQuote(ReceiveQuoteInput data) {
-		// TODO do something
-		
-		// fetch quote write model by id
-		// verify if server checks out
-		// verify if verdict checks out
-		// add receive
-		// return
+
+	public void receiveQuote(ReceiveQuoteInput data) throws InterruptedException, ExecutionException, IOException {
+		var projection = projSvc.getProjection(data.getQuoteId());
+		if (projection == null) {
+			// TODO throw error
+			return;
+		}
+
+		var projModel = projection.getFirst();
+		if (projModel.getServerId() != data.getServerId()) {
+			// TODO throw error
+			return;
+		}
+
+		var writeModel = writeRepo.convertToWriteModel(projModel, projection.getSecond());
+		writeModel.receive(UUID.randomUUID().toString(), data.getReceiverId(), Instant.now(), data.getServerId(),
+				data.getChannelId(), data.getMessageId());
+		writeRepo.saveWriteModel(writeModel);
 	}
 }
