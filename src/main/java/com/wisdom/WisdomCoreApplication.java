84b1db7e4939819b2b719e167da6afced86a0ece
Felix <jonfelixrico@gmail.com>
2022-10-06 00:27:19 +0800

    Remove the CRON job

--- src/main/java/com/wisdom/WisdomCoreApplication.java
+++ src/main/java/com/wisdom/WisdomCoreApplication.java
@@ -3,11 +3,9 @@ package com.wisdom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableMongoRepositories
-@EnableScheduling
 public class WisdomCoreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/wisdom/quote/service/PendingQuoteService.java
+++ src/main/java/com/wisdom/quote/service/PendingQuoteService.java
@@ -8,7 +8,6 @@ import java.util.concurrent.ExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.wisdom.quote.projection.QuoteProjectionService;
@@ -80,33 +79,5 @@ public class PendingQuoteService {
 		var writeModel = writeRepo.convertToWriteModel(projModel, projection.getSecond());
 		writeModel.approveBySystem(Instant.now());
 		writeRepo.saveWriteModel(writeModel);
-	}
-
-	@Deprecated
-	@Scheduled(cron = "0 */5 * * * *")
-	private void doExpirationFlagging() {
-		var ids = mongoRepo.getAllPendingQuotesForExpirationFlagging(Instant.now()).stream().map(i -> i.getId())
-				.toList();
-
-		if (ids.size() == 0) {
-			LOGGER.debug("Prematurely terminated expired quotes sweep: no expired quotes found");
-			return;
-		}
-
-		LOGGER.debug("Attempting to flag {} quotes as expired", ids.size());
-		int successCount = 0;
-		for (String id : ids) {
-			try {
-				var writeModel = writeRepo.getWriteModel(id);
-				writeModel.flagAsExpired(Instant.now());
-				writeRepo.saveWriteModel(writeModel);
-				LOGGER.debug("Flagged {} as expired", id);
-				successCount++;
-			} catch (Exception e) {
-				LOGGER.error("An exception was encountered while trying to flag {} as expired", id, e);
-			}
-		}
-
-		LOGGER.debug("Succesfully flagged {} out of {} quotes as expired", successCount, ids.size());
 	}
 }
