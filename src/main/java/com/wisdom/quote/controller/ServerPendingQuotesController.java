17e96f7ea0df40faf4f48ced5472a2c401769998
Felix <jonfelixrico@gmail.com>
2022-10-06 22:39:29 +0800

    Use the new services in the controllers

--- src/main/java/com/wisdom/quote/controller/ServerPendingQuotesController.java
+++ src/main/java/com/wisdom/quote/controller/ServerPendingQuotesController.java
@@ -4,7 +4,6 @@
 package com.wisdom.quote.controller;
 
 import java.io.IOException;
-import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +13,7 @@ import java.util.concurrent.ExecutionException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +21,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
+import com.wisdom.common.service.TimeService;
 import com.wisdom.quote.controller.dto.SubmitQuoteReqDto;
+import com.wisdom.quote.projection.QuoteProjection;
+import com.wisdom.quote.projection.QuoteProjectionServiceV2;
 import com.wisdom.quote.service.PendingQuoteServiceModel;
-import com.wisdom.quote.service.PendingQuoteService;
-import com.wisdom.quote.writemodel.QuoteWriteModelRepository;
+import com.wisdom.quote.writemodel.QuoteWriteService;
 
 /**
  * @author Felix
@@ -34,53 +37,89 @@ import com.wisdom.quote.writemodel.QuoteWriteModelRepository;
 @RestController
 @RequestMapping("/server/{serverId}/quote/pending")
 public class ServerPendingQuotesController {
-	@Autowired
-	private QuoteWriteModelRepository writeRepo;
 
 	@Autowired
-	private PendingQuoteService pendingQuoteSvc;
+	private QuoteWriteService writeSvc;
+
+	@Autowired
+	private TimeService timeSvc;
+
+	@Autowired
+	private QuoteProjectionServiceV2 projSvc;
 
 	@PostMapping
 	private Map<String, String> submitQuote(@Valid @RequestBody SubmitQuoteReqDto body) throws Exception {
 		var quoteId = UUID.randomUUID().toString();
-		var createDt = Instant.now();
+		var createDt = timeSvc.getCurrentTime();
 		var expireDt = createDt.plus(3, ChronoUnit.DAYS);
 
-		var writeModel = writeRepo.create(quoteId, body.getContent(), body.getAuthorId(), body.getSubmitterId(),
-				createDt, expireDt, body.getServerId(), body.getChannelId(), body.getMessageId());
-
-		writeRepo.saveWriteModel(writeModel);
+		var writeModel = writeSvc.create(quoteId, body.getContent(), body.getAuthorId(), body.getSubmitterId(),
+				createDt, expireDt, body.getServerId(), body.getChannelId(), body.getMessageId(), 3);
+		writeModel.save();
 
 		return Map.of("quoteId", quoteId);
 	}
 
-	@PutMapping("/{id}/vote")
-	private void setVotes(@PathVariable String id, @RequestBody List<String> voterIds)
-			throws InterruptedException, ExecutionException, IOException {
-		var model = writeRepo.getWriteModel(id);
-		model.setVoters(voterIds, Instant.now());
-		writeRepo.saveWriteModel(model);
+	@PutMapping("/{quoteId}/vote")
+	private void setVotes(@PathVariable String quoteId, @PathVariable String serverId,
+			@RequestBody List<String> voterIds) throws Exception {
+		var writeModel = writeSvc.get(quoteId);
+		if (!writeModel.getEntity().getServerId().equals(serverId)) {
+			throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+		}
+
+		try {
+			writeModel.updateVotingSession(voterIds, timeSvc.getCurrentTime());
+			writeModel.save();
+		} catch (IllegalStateException e) {
+			throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
+		}
 	}
 
-	@GetMapping("/{id}")
-	private PendingQuoteServiceModel getPendingQuote(@PathVariable String serverId, @PathVariable String id) {
-		return pendingQuoteSvc.getPendingQuote(id, serverId);
+	@GetMapping("/{quoteId}")
+	private QuoteProjection getPendingQuote(@PathVariable String serverId, @PathVariable String quoteId)
+			throws InterruptedException, ExecutionException, IOException {
+		var proj = projSvc.getProjection(quoteId);
+		if (!proj.getServerId().equals(serverId) || proj.getVerdict() != null) {
+			throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+		}
+
+		return proj;
 	}
 
 	@GetMapping
 	private List<PendingQuoteServiceModel> getPendingQuotes(@PathVariable String serverId) {
-		return pendingQuoteSvc.getPendingQuotes(serverId);
+		// TODO restore this
+		return null;
 	}
 
 	@PutMapping("/{quoteId}/expire")
-	private void flagQuoteAsDeleted(@PathVariable String serverId, @PathVariable String quoteId)
-			throws InterruptedException, ExecutionException, IOException {
-		pendingQuoteSvc.flagQuoteAsExpired(quoteId, serverId);
+	private void flagQuoteAsDeleted(@PathVariable String serverId, @PathVariable String quoteId) throws Exception {
+		var writeModel = writeSvc.get(quoteId);
+		if (!writeModel.getEntity().getServerId().equals(serverId) || writeModel.getEntity().getVerdict() != null) {
+			throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+		}
+
+		try {
+			writeModel.flagAsExpiredBySystem(timeSvc.getCurrentTime());
+			writeModel.save();
+		} catch (IllegalStateException e) {
+			throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
+		}
 	}
 
 	@PutMapping("/{quoteId}/approve")
-	private void approveQuote(@PathVariable String serverId, @PathVariable String quoteId)
-			throws InterruptedException, ExecutionException, IOException {
-		pendingQuoteSvc.approveQuote(quoteId, serverId);
+	private void approveQuote(@PathVariable String serverId, @PathVariable String quoteId) throws Exception {
+		var writeModel = writeSvc.get(quoteId);
+		if (!writeModel.getEntity().getServerId().equals(serverId) || writeModel.getEntity().getVerdict() != null) {
+			throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+		}
+
+		try {
+			writeModel.approveBySystem(timeSvc.getCurrentTime());
+			writeModel.save();
+		} catch (IllegalStateException e) {
+			throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
+		}
 	}
 }
