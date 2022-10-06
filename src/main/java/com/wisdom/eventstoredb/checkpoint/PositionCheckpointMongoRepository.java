63030a4eb225c1b8da476865e68857ef716e70c4
Felix <jonfelixrico@gmail.com>
2022-10-06 22:44:11 +0800

    Remove deprecated/unused code

--- src/main/java/com/wisdom/eventstoredb/checkpoint/PositionCheckpointMongoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.wisdom.eventstoredb.checkpoint;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-interface PositionCheckpointMongoRepository extends MongoRepository<PositionCheckpointMongoModel, String> {
-}
