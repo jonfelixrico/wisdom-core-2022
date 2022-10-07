a8d60af20a2d163fe0cfff48952f4f112ab36053
Felix <jonfelixrico@gmail.com>
2022-10-07 23:32:50 +0800

    Rename the status declaration event

--- src/main/java/com/wisdom/quote/writemodel/events/QuoteStatusDeclaredEvent.java
+++ src/main/java/com/wisdom/quote/writemodel/events/QuoteStatusDeclaredEvent.java
@@ -2,13 +2,13 @@ package com.wisdom.quote.writemodel.events;
 
 import java.time.Instant;
 
-public class QuoteStatusDeclared extends BaseQuoteEvent {
+public class QuoteStatusDeclaredEvent extends BaseQuoteEvent {
 	public static final String EVENT_TYPE = "QUOTE_STATUS_DECLARED";
 
 	private String quoteId;
 	private Instant timestamp;
 
-	public QuoteStatusDeclared(String quoteId, Instant timestamp) {
+	public QuoteStatusDeclaredEvent(String quoteId, Instant timestamp) {
 		this.quoteId = quoteId;
 		this.timestamp = timestamp;
 	}
