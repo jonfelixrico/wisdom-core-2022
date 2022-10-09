b6ead462ff0da231c155c8bddf1b26b3414e66d8
Felix <jonfelixrico@gmail.com>
2022-10-09 16:20:00 +0800

    Add the forgotten Autowired usages

--- src/main/java/com/wisdom/quote/readmodel/QuoteReadModelRepository.java
+++ src/main/java/com/wisdom/quote/readmodel/QuoteReadModelRepository.java
@@ -3,10 +3,12 @@ package com.wisdom.quote.readmodel;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuoteReadModelRepository {
+	@Autowired
 	private QuoteReadMDBRepository repo;
 
 	public QuoteReadModel findById(String quoteId) {
