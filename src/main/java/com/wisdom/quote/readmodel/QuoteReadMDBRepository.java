67729b23e75d9f51859e8e3ba0ffa840e7cdd780
Felix <jonfelixrico@gmail.com>
2022-10-10 02:39:03 +0800

    Change the pipeline sequence

--- src/main/java/com/wisdom/quote/readmodel/QuoteReadMDBRepository.java
+++ src/main/java/com/wisdom/quote/readmodel/QuoteReadMDBRepository.java
@@ -7,14 +7,14 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 interface QuoteReadMDBRepository extends MongoRepository<QuoteReadMDB, String> {
-	@Query("{ serverId: ?0, 'statusDeclaration': null }")
-	public List<QuoteReadMDB> getPendingQuotesByServerId(String serverId);
+  @Query("{ serverId: ?0, 'statusDeclaration': null }")
+  public List<QuoteReadMDB> getPendingQuotesByServerId(String serverId);
 
-	/**
-	 * 
-	 * @return This list will be empty or will contain only one item.
-	 */
-	@Aggregation(pipeline = { "{ $sample: { size: 1 }}",
-			"{ $match: { serverId: ?0, 'statusDeclaration.status': 'APPROVED' } }" })
-	public List<QuoteReadMDB> getRandomQuoteByServerId(String serverId);
+  /**
+   * 
+   * @return This list will be empty or will contain only one item.
+   */
+  @Aggregation(pipeline = {
+      "{ $match: { serverId: ?0, 'statusDeclaration.status': 'APPROVED' } }", "{ $sample: { size: 1 }}" })
+  public List<QuoteReadMDB> getRandomQuoteByServerId(String serverId);
 }
