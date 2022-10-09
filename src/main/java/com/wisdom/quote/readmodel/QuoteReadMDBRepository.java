7d769c88eeaf68fb9dc7b2d45a1494685387b155
Felix <jonfelixrico@gmail.com>
2022-10-09 16:05:41 +0800

    Implement the listing methods

--- src/main/java/com/wisdom/quote/readmodel/QuoteReadMDBRepository.java
+++ src/main/java/com/wisdom/quote/readmodel/QuoteReadMDBRepository.java
@@ -1,7 +1,20 @@
 package com.wisdom.quote.readmodel;
 
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 interface QuoteReadMDBRepository extends MongoRepository<QuoteReadMDB, String> {
+	@Query("{ serverId: ?0, 'statusDeclaration': null }")
+	public List<QuoteReadMDB> getPendingQuotesByServerId(String serverId);
 
+	/**
+	 * 
+	 * @return This list will be empty or will contain only one item.
+	 */
+	@Aggregation(pipeline = { "{ $sample: { size: 1 }}",
+			"{ $match: { serverId: ?0, 'statusDeclaration.status': 'APPROVED' } }" })
+	public List<QuoteReadMDB> getRandomQuoteByServerId(String serverId);
 }
--- src/main/java/com/wisdom/quote/readmodel/QuoteReadModelRepository.java
+++ src/main/java/com/wisdom/quote/readmodel/QuoteReadModelRepository.java
@@ -1,13 +1,25 @@
 package com.wisdom.quote.readmodel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuoteReadModelRepository {
 	private QuoteReadMDBRepository repo;
-	
+
 	public QuoteReadModel findById(String quoteId) {
 		var results = repo.findById(quoteId);
 		return results.isEmpty() ? null : results.get();
+	}
+
+	public List<QuoteReadModel> findPendingQuotesInServer(String serverId) {
+		return new ArrayList<QuoteReadModel>(repo.getPendingQuotesByServerId(serverId));
+	}
+
+	public QuoteReadModel getRandomQuoteInServer(String serverId) {
+		var fromDb = repo.getRandomQuoteByServerId(serverId);
+		return fromDb.isEmpty() ? null : fromDb.get(0);
 	}
 }
