ee45656ce3fd3861917029ac8e53a2f22d2ec9f3
Felix <jonfelixrico@gmail.com>
2022-10-14 01:19:38 +0800

    Push adjustments to QuoteProjection and its subclasses

--- src/main/java/com/wisdom/quote/writemodel/QuoteSnapshotDocument.java
+++ src/main/java/com/wisdom/quote/writemodel/QuoteSnapshotDocument.java
@@ -2,6 +2,7 @@ package com.wisdom.quote.writemodel;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.annotation.PersistenceCreator;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,20 +10,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import com.wisdom.quote.entity.QuoteEntity;
 import com.wisdom.quote.entity.Receive;
 import com.wisdom.quote.entity.StatusDeclaration;
-import com.wisdom.quote.entity.VotingSession;
 
 @Document("quote-snapshot")
 class QuoteSnapshotDocument extends QuoteProjection {
-	@PersistenceCreator
-	public QuoteSnapshotDocument(String id, String content, String authorId, String submitterId, Instant submitDt,
-			Instant expirationDt, String serverId, String channelId, String messageId, List<Receive> receives,
-			StatusDeclaration statusDeclaration, VotingSession votingSession, Integer requiredVoteCount,
-			Long revision) {
-		super(id, content, authorId, submitterId, submitDt, expirationDt, serverId, channelId, messageId, receives,
-				statusDeclaration, votingSession, requiredVoteCount, revision);
-	}
+  @PersistenceCreator
+  public QuoteSnapshotDocument(String id, String content, String authorId, String submitterId, Instant submitDt,
+      Instant expirationDt, String serverId, String channelId, String messageId, List<Receive> receives,
+      StatusDeclaration statusDeclaration, Map<String, Instant> votes, Integer requiredVoteCount, Long revision) {
+    super(id, content, authorId, submitterId, submitDt, expirationDt, serverId, channelId, messageId, receives,
+        statusDeclaration, votes, requiredVoteCount, revision);
+    // TODO Auto-generated constructor stub
+  }
 
-	QuoteSnapshotDocument(QuoteEntity base, long revision) {
-		super(base, revision);
-	}
+  public QuoteSnapshotDocument(QuoteEntity base, long revision) {
+    super(base, revision);
+  }
 }
