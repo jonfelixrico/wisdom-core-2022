1cc8d294d3d78f8d73a95b778fb95adbc37fa24d
Felix <jonfelixrico@gmail.com>
2022-10-14 02:11:08 +0800

    Allow the /quote/random route to be filterable by author id

--- src/main/java/com/wisdom/quote/controller/QuotesReadController.java
+++ src/main/java/com/wisdom/quote/controller/QuotesReadController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wisdom.quote.readmodel.QuoteReadModel;
@@ -12,11 +13,15 @@ import com.wisdom.quote.readmodel.QuoteReadModelRepository;
 @RestController
 @RequestMapping("/server/{serverId}/quote")
 public class QuotesReadController {
-	@Autowired
-	private QuoteReadModelRepository repo;
+  @Autowired
+  private QuoteReadModelRepository repo;
 
-	@GetMapping("/random")
-	private QuoteReadModel getRandomQuote(@PathVariable String serverId) {
-		return repo.getRandomQuoteInServer(serverId);
-	}
+  @GetMapping("/random")
+  private QuoteReadModel getRandomQuote(@PathVariable String serverId, @RequestParam(required = false) String authorId) {
+    if (authorId == null) {
+      return repo.getRandomQuoteInServer(serverId);
+    }
+    
+    return repo.getRandomQuoteInServerFilteredByAuthor(serverId, authorId);
+  }
 }
