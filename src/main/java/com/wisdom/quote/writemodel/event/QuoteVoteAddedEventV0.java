9e11f1b7ed1b3aef208c0024ccc0b5b88f6a847a
Felix <jonfelixrico@gmail.com>
2022-11-12 23:35:54 +0800

    Create V0 version

---
+++ src/main/java/com/wisdom/quote/writemodel/event/QuoteVoteAddedEventV0.java
@@ -0,0 +1,27 @@
+package com.wisdom.quote.writemodel.event;
+
+import java.time.Instant;
+
+public class QuoteVoteAddedEventV0 extends BaseQuoteVoteAddedEvent {
+  /**
+   * Negative value for a downvote, positive value for an upvote.
+   */
+  private Integer value;
+
+  public QuoteVoteAddedEventV0(String quoteId, String userId, Instant timestamp, Integer value) {
+    super(quoteId, userId, timestamp);
+    this.value = value;
+  }
+
+  public static final String EVENT_TYPE = "QUOTE_VOTE_ADDED.V0";
+
+  @Override
+  public String getEventType() {
+    return EVENT_TYPE;
+  }
+
+  public Integer getValue() {
+    return value;
+  }
+
+}
