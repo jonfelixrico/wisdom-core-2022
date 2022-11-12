91daee94e374c2c5749298c00da745aadd05a1e6
Felix <jonfelixrico@gmail.com>
2022-11-13 00:05:14 +0800

    Make V1 extend V0

--- src/main/java/com/wisdom/quote/writemodel/event/QuoteReceivedEventV1.java
+++ src/main/java/com/wisdom/quote/writemodel/event/QuoteReceivedEventV1.java
@@ -2,49 +2,17 @@ package com.wisdom.quote.writemodel.event;
 
 import java.time.Instant;
 
-public class QuoteReceivedEventV1 extends BaseQuoteEvent {
+public class QuoteReceivedEventV1 extends QuoteReceivedEventV0 {
 	public static final String EVENT_TYPE = "QUOTE_RECEIVED.V1";
 
-	private String quoteId;
-	private String receiveId;
-
-	private String userId;
-	private Instant timestamp;
-
-	private String serverId;
 	private String channelId;
 	private String messageId;
 
 	public QuoteReceivedEventV1(String quoteId, String receiveId, String userId, Instant timestamp, String serverId,
 			String channelId, String messageId) {
-		this.quoteId = quoteId;
-		this.receiveId = receiveId;
-		this.userId = userId;
-		this.timestamp = timestamp;
-		this.serverId = serverId;
+	    super(quoteId, receiveId, userId, timestamp, serverId);
 		this.channelId = channelId;
 		this.messageId = messageId;
-	}
-
-	public Instant getTimestamp() {
-		return timestamp;
-	}
-
-	@Override
-	public String getQuoteId() {
-		return quoteId;
-	}
-
-	public String getReceiveId() {
-		return receiveId;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public String getServerId() {
-		return serverId;
 	}
 
 	public String getChannelId() {
