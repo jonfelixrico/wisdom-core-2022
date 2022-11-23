00207d971b233a7f04ecddb01c9226c295da561b
Felix <jonfelixrico@gmail.com>
2022-11-24 00:39:21 +0800

    Make QuoteBehavior methods public

--- src/main/java/com/wisdom/quote/entity/QuoteBehavior.java
+++ src/main/java/com/wisdom/quote/entity/QuoteBehavior.java
@@ -3,17 +3,28 @@ package com.wisdom.quote.entity;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class QuoteBehavior extends QuoteEntity {
   private static Logger LOGGER = LoggerFactory.getLogger(QuoteBehavior.class);
 
-  protected QuoteBehavior(QuoteEntity entity) {
+  public QuoteBehavior(String id, String content, String authorId, String submitterId, Instant submitDt,
+      Instant expirationDt, String serverId, String channelId, String messageId, List<Receive> receives,
+      StatusDeclaration statusDeclaration, Map<String, Instant> votes, Integer requiredVoteCount, Boolean isLegacy) {
+    super(id, content, authorId, submitterId, submitDt, expirationDt, serverId, channelId, messageId, receives,
+        statusDeclaration, votes, requiredVoteCount, isLegacy);
+    // TODO Auto-generated constructor stub
+  }
+
+  public QuoteBehavior(QuoteEntity entity) {
     super(entity);
   }
 
-  protected void receive(Receive receive) {
+  public void receive(Receive receive) {
     if (getStatusDeclaration() == null || getStatusDeclaration().getStatus() != Status.APPROVED) {
       throw new IllegalStateException("Quote does not accept receives.");
     }
@@ -23,7 +34,7 @@ public abstract class QuoteBehavior extends QuoteEntity {
     setReceives(clone);
   }
 
-  protected void declareStatus(StatusDeclaration declaration) {
+  public void declareStatus(StatusDeclaration declaration) {
     if (getStatusDeclaration() != null) {
       throw new IllegalStateException("Quote already has a status.");
     }
@@ -31,7 +42,7 @@ public abstract class QuoteBehavior extends QuoteEntity {
     setStatusDeclaration(declaration);
   }
 
-  protected void addVote(String userId, Instant timestamp) {
+  public void addVote(String userId, Instant timestamp) {
     if (getVotes().containsKey(userId)) {
       throw new IllegalStateException("User has already voted.");
     }
@@ -42,7 +53,7 @@ public abstract class QuoteBehavior extends QuoteEntity {
     setVotes(clone);
   }
 
-  protected void removeVote(String userId) {
+  public void removeVote(String userId) {
     if (!getVotes().containsKey(userId)) {
       LOGGER.warn("Tried to remove nonexistent vote for user {} for quote {}", userId, getId());
       return;
