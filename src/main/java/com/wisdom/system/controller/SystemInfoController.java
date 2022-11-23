5e4ed1c6c555b7a63d7a1bcd933ac5a5918fd73c
Felix <jonfelixrico@gmail.com>
2022-11-24 02:00:03 +0800

    Revert "Adjust the controller"
    This reverts commit 96e882cd38e66af62f136690a428c360871deb26.

--- src/main/java/com/wisdom/system/controller/SystemInfoController.java
+++ src/main/java/com/wisdom/system/controller/SystemInfoController.java
@@ -5,7 +5,7 @@ import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wisdom.system.controller.dto.SystemInfoDto;
+import com.wisdom.system.controller.dto.VersionRespDto;
 
 @RestController
 public class SystemInfoController {
@@ -13,8 +13,8 @@ public class SystemInfoController {
   @Autowired
   private BuildProperties build;
 
-  @GetMapping
-  public SystemInfoDto getVersion() {
-    return new SystemInfoDto(build.getVersion());
+  @GetMapping("/version")
+  public VersionRespDto getVersion() {
+    return new VersionRespDto(build.getVersion());
   }
 }
