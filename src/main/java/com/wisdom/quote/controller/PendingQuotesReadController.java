0a55a0558ba1b02aaa2f60149debceddbfd81e93
Felix <jonfelixrico@gmail.com>
2023-01-03 23:46:44 +0800

    Add api documentation to pendingquotereadcontroller

--- src/main/java/com/wisdom/quote/controller/PendingQuotesReadController.java
+++ src/main/java/com/wisdom/quote/controller/PendingQuotesReadController.java
@@ -10,11 +10,21 @@ import org.springframework.web.server.ResponseStatusException;
 import com.wisdom.quote.readmodel.QuoteSnapshot;
 import com.wisdom.quote.readmodel.QuoteSnapshotRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 public class PendingQuotesReadController {
   @Autowired
   private QuoteSnapshotRepository repo;
 
+  @Operation(operationId = "getPendingQuote", summary = "Get a pending quote")
+  @ApiResponses(value = {
+      @ApiResponse(),
+      @ApiResponse(responseCode = "404", description = "Pending quote not found", content = { @Content() })
+  })
   @GetMapping("/pending-quote/{quoteId}")
   private QuoteSnapshot getPendingQuote(@PathVariable String quoteId) {
     var data = repo.findById(quoteId);
