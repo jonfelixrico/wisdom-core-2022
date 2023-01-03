633fe5079f77cf0ec392a7e9b9146affbd1921e6
Felix <jonfelixrico@gmail.com>
2023-01-04 00:03:56 +0800

    Add API docu

--- src/main/java/com/wisdom/system/controller/SystemInfoController.java
+++ src/main/java/com/wisdom/system/controller/SystemInfoController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.wisdom.system.controller.dto.VersionRespDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class SystemInfoController {
 
   @Autowired
   private BuildProperties build;
 
+  @Operation(operationId = "getAppVersion", summary = "Get the Wisdom Core version")
   @GetMapping("/version")
   public VersionRespDto getVersion() {
     return new VersionRespDto(build.getVersion());
