b065dfd5cb54221585b3e9e5565f8440d68bc7bc
Felix <jonfelixrico@gmail.com>
2023-01-27 00:14:24 +0800

    Create listServerQuotes

--- src/main/java/com/wisdom/quote/readmodel/QuoteSnapshotRepository.java
+++ src/main/java/com/wisdom/quote/readmodel/QuoteSnapshotRepository.java
@@ -5,12 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.MongoExpression;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationExpression;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuoteSnapshotRepository {
   @Autowired
   private QuoteSnapshotPersistenceRepository repo;
+
+  @Autowired
+  private MongoTemplate mongoTemplate;
 
   public QuoteSnapshot findById(String quoteId) {
     var results = repo.findById(quoteId);
@@ -41,5 +50,59 @@ public class QuoteSnapshotRepository {
 
   public List<QuoteSnapshot> getServerQuotes(String serverId, String authorId) {
     return new ArrayList<>(repo.getRandomQuoteByServerIdAndAuthorId(serverId, authorId));
+  }
+
+  /**
+   * Lists the pending or approved quotes from the server.
+   * Uses cursor-based pagination.
+   * The pagination is sorted descending by submission date.
+   * 
+   * @param serverId
+   * @param limit
+   * @param after Takes the quotes after the specified "cursor". If null, takes the quotes from the start of the results.
+   * @return
+   */
+  public List<QuoteSnapshot> listServerQuotes(String serverId, int limit, String after) {
+    // get only the approved or pending quotes from the specified server
+    var matchExpression = MongoExpression
+        .create("{ serverId: ?0, statusDeclaration: { $or: [null, { status: 'APPROVED' }] } }", serverId);
+    var matchStage = Aggregation.match(AggregationExpression.from(matchExpression));
+    var sortStage = Aggregation.sort(Sort.by(Sort.Order.desc("submitDt")));
+    var aggregate = Aggregation.newAggregation(matchStage, sortStage);
+
+    /*
+     * If after is not provided, then we will start taking from the result set
+     * immediately.
+     * 
+     * Else, this will be false. This will only be true AFTER we've found the
+     * "cursor".
+     * We start taking items after the cursor.
+     * 
+     * (taking = pushing into the list)
+     */
+    boolean isCursorFound = after == null;
+
+    var list = new ArrayList<QuoteSnapshot>();
+    try (var resultSet = mongoTemplate.aggregateStream(aggregate, QuoteSnapshotPersistence.class,
+        QuoteSnapshotPersistence.class)) {
+      while (resultSet.hasNext() ||
+      // the limiting mechanism
+          list.size() <= limit) {
+        var value = resultSet.next();
+        if (!isCursorFound) {
+          /*
+           * If we have found the "cursor", then it's time to start pushing items into the
+           * list.
+           * This will start in the iteration after the one where the cursor was found.
+           */
+          isCursorFound = value.getId() == after;
+          continue;
+        }
+
+        list.add(value);
+      }
+    }
+
+    return list;
   }
 }
