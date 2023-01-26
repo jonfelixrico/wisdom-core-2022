95b0e8a16af16c39643e0f533511b2ddee4c9ac0
Felix <jonfelixrico@gmail.com>
2023-01-27 01:09:31 +0800

    Issue #46: Implement quote list endpoint (#47)
    * Create stub for /server//{serverId}/quotes V2
    
    * Create listServerQuotes
    
    * Apply minor changes
    
    * Remove unused imports
    
    * Fix the fetching logic
    
    * Implement the endpoint

---
+++ src/main/java/com/wisdom/quote/controller/v2/ServerQuotesReadControllerV2.java
@@ -0,0 +1,31 @@
+package com.wisdom.quote.controller.v2;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.wisdom.quote.readmodel.QuoteSnapshot;
+import com.wisdom.quote.readmodel.QuoteSnapshotRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "server-quotes")
+@RestController
+@RequestMapping("/v2/server/{serverId}/quote")
+public class ServerQuotesReadControllerV2 {
+  @Autowired
+  private QuoteSnapshotRepository repo;
+
+  @Operation(operationId = "getServerQuotesV2", summary = "List the quotes of a server (includes approved and pending quotes)")
+  @GetMapping
+  private List<QuoteSnapshot> getServerQuotes(@PathVariable String serverId,
+      @RequestParam(defaultValue = "20") Integer limit,
+      @RequestParam(required = false) String after) {
+    return repo.listServerQuotes(serverId, limit, after);
+  }
+}
